eb2d81ad224f53259df80b123c2bac4b0834e643
ErikBabajanyan <Erik.Babajanyan10@gmail.com>
2024-04-22 20:50:59 +0400

    added emoji

--- bot.js
+++ bot.js
@@ -4,7 +4,6 @@ const transactionModel = require("./schema/transaction.schema");
 const TimeAgo = require("javascript-time-ago");
 const hy = require("javascript-time-ago/locale/hy");
 const ARMENIA_TIME = require("./util/armenian_time");
-const fetchData = require("./gecko.transaction");
 require("dotenv").config();
 
 TimeAgo.addLocale(hy);
@@ -12,40 +11,47 @@ TimeAgo.addLocale(hy);
 const BOT_ID = process.env.BOT_ID;
 const bot = new Telegraf(BOT_ID);
 const GROUPP_CHAT_ID = process.env.GROUPP_CHAT_ID;
-const CURRENT_DATE = new Date();
+const CURRENT_DATE = ARMENIA_TIME(new Date());
 const ATLAS_URL = process.env.ATLAS_URL;
 const axios = require("axios");
-
+const API_URL = process.env.API_URL;
+console.log(CURRENT_DATE);
 async function mongo() {
   try {
     const connection = await mongoose.connect(ATLAS_URL);
     console.log(
       "MongoDB connection established:",
       connection.connection.readyState
-    ); // Improved logging with connection state
-
-    // Add logic to handle successful connection here (e.g., initialize your Telegram bot)
+    );
   } catch (error) {
     console.error("MongoDB Connection Failed:", error.message);
-
-    // Implement robust error handling here (e.g., retry logic, error notification)
   }
 }
 
 mongo();
 const activeChatId = {};
 
+async function fetchData() {
+  try {
+    const response = await axios.get(API_URL, {
+      headers: {
+        accept: "application/json",
+      },
+    });
+    const data = response.data.data;
+    return data;
+  } catch (error) {
+    console.error("Error fetching data:", error);
+  }
+}
+
 async function sendNewTransaction(walletAddress) {
-  console.log(11);
   try {
     const myWallet = await transactionModel.find({ wallet: walletAddress });
     console.log(myWallet);
     const wallet = myWallet[0];
     const lastTransactionDate =
       wallet.transactions[wallet.transactions.length - 1].date;
-    if (new Date(lastTransactionDate) < CURRENT_DATE) {
-      return;
-    }
     console.log(wallet.wallet, 11);
     let x = "";
     let b = "";
@@ -59,22 +65,21 @@ async function sendNewTransaction(walletAddress) {
       } else {
         type = "❌";
       }
-      let fishEmoji = "";
-      let g = transaction.amount / 1000;
-      if (transaction.amount > 999) {
-        for (let i = 0; i < Math.floor(g); i++) {
-          fishEmoji = fishEmoji + "🐟";
-        }
-      }
 
       return (x =
         x +
-        fishEmoji +
-        `\n\n` +
+        `\n` +
         `💲 Amount: ${transaction.amount}\n` +
         `📅 date: ${transaction.date}\n` +
         `${type} type: ${transaction.type}\n\n`);
     });
+    let fishEmoji = "";
+    if (wallet.transactions[wallet.transactions.length - 1].amount > 999) {
+      let g = wallet.transactions[wallet.transactions.length - 1].amount / 1000;
+      for (let i = 0; i < Math.floor(g); i++) {
+        fishEmoji = fishEmoji + "🐟";
+      }
+    }
     console.log(x);
 
     console.log(wallet.intervalBuy);
@@ -94,17 +99,19 @@ async function sendNewTransaction(walletAddress) {
       wallet.balance
     } = USD (${
       wallet.balance * wallet.price
-    })\n\n${x}📈 intervalBuy: ${b}\📉 nintervalSell: ${s}\nbuy count: ${
+    })\n${fishEmoji}\n${x}📈 intervalBuy: ${b}\n📉 intervalSell: ${s}\nbuy count: ${
       wallet.buyCount
     }\nsell count: ${wallet.sellCount}`;
 
     const activeChats = Object.keys(activeChatId);
-
-    if (new Date(lastTransactionDate) > CURRENT_DATE) {
-      await bot.telegram.sendMessage(GROUPP_CHAT_ID, message);
-    }
+    console.log(activeChatId);
+    // if (new Date(lastTransactionDate) > new Date(CURRENT_DATE)) {
+    //   console.log(GROUPP_CHAT_ID);
+    //   await bot.telegram.sendMessage(GROUPP_CHAT_ID, message);
+    // }
     activeChats.map(async (key) => {
-      if (new Date(lastTransactionDate) > activeChatId[key]) {
+      console.log(key);
+      if (new Date(lastTransactionDate) > new Date(activeChatId[key])) {
         await bot.telegram.sendMessage(key, message);
       }
     });
@@ -146,6 +153,7 @@ async function getAccount(account_id) {
 async function transactions() {
   try {
     const data = await fetchData();
+    if (!data) return;
     data.reverse();
     for (const item of data) {
       if (
@@ -153,131 +161,122 @@ async function transactions() {
           item.attributes.volume_in_usd > 499) ||
         item.attributes.volume_in_usd > 999
       ) {
-        if (CURRENT_DATE < new Date(item.attributes.block_timestamp)) {
-          const walletAddress = item.attributes.tx_from_address;
-          let price = 0;
-          if (item.attributes.kind === "buy") {
-            price = Number(item.attributes.price_from_in_usd);
-          } else {
-            price = Number(item.attributes.price_to_in_usd);
-          }
-          const transaction = {
-            hash: item.attributes.tx_hash,
-            amount: item.attributes.volume_in_usd,
-            date: ARMENIA_TIME(item.attributes.block_timestamp),
-            type: item.attributes.kind,
-          };
-          const existingTransactions = await transactionModel.find({
-            wallet: item.attributes.tx_from_address,
-          });
+        const walletAddress = item.attributes.tx_from_address;
+        let price = 0;
+        if (item.attributes.kind === "buy") {
+          price = Number(item.attributes.price_from_in_usd);
+        } else {
+          price = Number(item.attributes.price_to_in_usd);
+        }
+        const transaction = {
+          hash: item.attributes.tx_hash,
+          amount: item.attributes.volume_in_usd,
+          date: ARMENIA_TIME(item.attributes.block_timestamp),
+          type: item.attributes.kind,
+        };
+        const existingTransactions = await transactionModel.find({
+          wallet: item.attributes.tx_from_address,
+        });
 
-          if (existingTransactions.length > 0) {
-            const firstTransaction = existingTransactions[0];
-            const matchingTransaction = firstTransaction.transactions.find(
-              (existingTxn) => existingTxn.hash === transaction.hash
-            );
+        if (existingTransactions.length > 0) {
+          const firstTransaction = existingTransactions[0];
+          const matchingTransaction = firstTransaction.transactions.find(
+            (existingTxn) => existingTxn.hash === transaction.hash
+          );
 
-            if (!matchingTransaction) {
-              const timeAgoHy = new TimeAgo("hy");
-              const newTransactionTime = new Date(transaction.date);
-              let lastSellTransaction;
-              let lastBuyTransaction;
-              if (item.attributes.type === "sell") {
-                console.log(1);
-                for (
-                  let i = firstTransaction.transactions.length - 1;
-                  i >= 0;
-                  i--
-                ) {
-                  const transaction = firstTransaction.transactions[i];
-                  console.log(transaction);
-                  if (transaction.type === "sell") {
-                    lastSellTransaction = transaction;
-                    break;
-                  }
-                }
-              } else {
-                console.log(2);
-                for (
-                  let i = firstTransaction.transactions.length - 1;
-                  i >= 0;
-                  i--
-                ) {
-                  const transaction = firstTransaction.transactions[i];
-                  console.log(transaction);
-                  if (transaction.type === "buy") {
-                    lastBuyTransaction = transaction;
-                    break;
-                  }
+          if (!matchingTransaction) {
+            const timeAgoHy = new TimeAgo("hy");
+            const newTransactionTime = new Date(transaction.date);
+            let lastSellTransaction;
+            let lastBuyTransaction;
+            if (item.attributes.type === "sell") {
+              console.log(1);
+              for (
+                let i = firstTransaction.transactions.length - 1;
+                i >= 0;
+                i--
+              ) {
+                const transaction = firstTransaction.transactions[i];
+                console.log(transaction);
+                if (transaction.type === "sell") {
+                  lastSellTransaction = transaction;
+                  break;
                 }
               }
-              if (lastBuyTransaction) {
-                const lastTransactionTime = new Date(lastBuyTransaction.date);
-                const intervalInMilliseconds =
-                  new Date(newTransactionTime) - lastTransactionTime;
-                const formattedInterval = timeAgoHy.format(
-                  Date.now() - intervalInMilliseconds
-                );
-                console.log(formattedInterval, "3");
-                firstTransaction.intervalBuy.push(formattedInterval);
-              } else if (lastSellTransaction) {
-                const lastTransactionTime = new Date(lastSellTransaction.date);
-                const intervalInMilliseconds =
-                  new Date(newTransactionTime) - lastTransactionTime;
-                const formattedInterval = timeAgoHy.format(
-                  Date.now() - intervalInMilliseconds
-                );
-                console.log(formattedInterval, "4");
-                firstTransaction.intervalSell.push(formattedInterval);
-              } else {
-                console.log("no have transaction");
-              }
-
-              const lastTransactionTime = new Date(
-                firstTransaction.transactions[
-                  firstTransaction.transactions.length - 1
-                ].date
-              );
-
-              firstTransaction.transactions.push(transaction);
-
-              if (item.attributes.kind === "buy") {
-                firstTransaction.buyCount += 1;
-              } else {
-                firstTransaction.sellCount += 1;
-              }
-
-              const balance = await getAccount(walletAddress);
-              firstTransaction.balance = balance;
-              firstTransaction.price = price;
-              await firstTransaction.save();
-              sendNewTransaction(walletAddress);
-              await new Promise((resolve) => setTimeout(resolve, 2000));
-            }
-          } else {
-            let buyCount = 0;
-            let sellCount = 0;
-            if (item.attributes.kind === "buy") {
-              buyCount = 1;
             } else {
-              sellCount = 1;
+              for (
+                let i = firstTransaction.transactions.length - 1;
+                i >= 0;
+                i--
+              ) {
+                const transaction = firstTransaction.transactions[i];
+                console.log(transaction);
+                if (transaction.type === "buy") {
+                  lastBuyTransaction = transaction;
+                  break;
+                }
+              }
+            }
+            if (lastBuyTransaction) {
+              const lastTransactionTime = new Date(lastBuyTransaction.date);
+              const intervalInMilliseconds =
+                new Date(newTransactionTime) - lastTransactionTime;
+              const formattedInterval = timeAgoHy.format(
+                Date.now() - intervalInMilliseconds
+              );
+              console.log(formattedInterval, "3");
+              firstTransaction.intervalBuy.push(formattedInterval);
+            } else if (lastSellTransaction) {
+              const lastTransactionTime = new Date(lastSellTransaction.date);
+              const intervalInMilliseconds =
+                new Date(newTransactionTime) - lastTransactionTime;
+              const formattedInterval = timeAgoHy.format(
+                Date.now() - intervalInMilliseconds
+              );
+              console.log(formattedInterval, "4");
+              firstTransaction.intervalSell.push(formattedInterval);
+            } else {
+              console.log("no have transaction");
+            }
+
+            firstTransaction.transactions.push(transaction);
+
+            if (item.attributes.kind === "buy") {
+              firstTransaction.buyCount += 1;
+            } else {
+              firstTransaction.sellCount += 1;
             }
 
             const balance = await getAccount(walletAddress);
-            const newTransaction = new transactionModel({
-              wallet: walletAddress,
-              balance: balance,
-              transactions: [transaction],
-              intervalBuy: [],
-              intervalSell: [],
-              buyCount: buyCount,
-              sellCount: sellCount,
-              price: price,
-            });
-            await newTransaction.save();
+            firstTransaction.balance = balance;
+            firstTransaction.price = price;
+            await firstTransaction.save();
             sendNewTransaction(walletAddress);
             await new Promise((resolve) => setTimeout(resolve, 2000));
           }
+        } else {
+          let buyCount = 0;
+          let sellCount = 0;
+          if (item.attributes.kind === "buy") {
+            buyCount = 1;
+          } else {
+            sellCount = 1;
+          }
+
+          const balance = await getAccount(walletAddress);
+          const newTransaction = new transactionModel({
+            wallet: walletAddress,
+            balance: balance,
+            transactions: [transaction],
+            intervalBuy: [],
+            intervalSell: [],
+            buyCount: buyCount,
+            sellCount: sellCount,
+            price: price,
+          });
+          await newTransaction.save();
+          sendNewTransaction(walletAddress);
+          await new Promise((resolve) => setTimeout(resolve, 2000));
         }
       }
     }
@@ -302,9 +301,10 @@ mainLoop();
 
 bot.start(async (ctx) => {
   const chatId = ctx.chat.id;
-  const armenianDate = new Date().toLocaleString("en-US", {
-    timeZone: "Asia/Yerevan",
-  });
+  const armenianDate = ARMENIA_TIME(new Date());
+  // const armenianDate = new Date().toLocaleString("en-US", {
+  //   timeZone: "Asia/Yerevan",
+  // });
   activeChatId[chatId] = armenianDate;
   console.log(activeChatId);
 });
--- gecko.transaction.js
+++ gecko.transaction.js
@@ -1,18 +1,3 @@
 require("dotenv").config();
-const axios = require("axios");
-const API_URL = process.env.API_URL;
-async function fetchData() {
-  try {
-    const response = await axios.get(API_URL, {
-      headers: {
-        accept: "application/json",
-      },
-    });
-    const data = response.data.data;
-    return data;
-  } catch (error) {
-    console.error("Error fetching data:", error);
-  }
-}
 
 module.exports = fetchData;
