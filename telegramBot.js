b65c559e9738dd6f260d4b9a9405d0f4a2d42133
ErikBabajanyan <Erik.Babajanyan10@gmail.com>
2024-04-22 23:13:07 +0400

    fix bot

--- telegramBot.js
+++ telegramBot.js
@@ -2,6 +2,6 @@
 const { Telegraf } = require("telegraf");
 
 const BOT_ID = process.env.BOT_ID;
-const bot = new Telegraf(BOT_ID);
+const bot = new Telegraf(BOT_ID, { polling: true });
 
 module.exports = bot;
